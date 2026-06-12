438bd46a819bf161b9e5f85f034e3a6bf1a09368
vikashyadav1815 <vikashyadav1815@gmail.com>
2026-06-12 15:53:37 +0530

    add new popular destinations image and update the styling of the WhyChooseUs component to match the new design. The changes include updating the colors, adding hover effects, and improving the overall layout and typography for better visual appeal.

--- src/components/home/PopularDestinations.jsx
+++ src/components/home/PopularDestinations.jsx
@@ -45,6 +45,12 @@ const destinationsData = [
     img: '/sikkim.png',
     desc: 'Pristine forests, glaciers, and stunning mountain vistas.',
   },
+  {
+    id: 7,
+    title: 'Arunachal Pradesh',
+    img: '/ap.png',
+    desc: 'Enchanting landscapes, diverse wildlife, and vibrant culture.',
+  }
 ]
 
 function PopularDestinations() {
